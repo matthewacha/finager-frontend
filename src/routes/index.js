19d913d27a956444e902e983bb57d68aa8a8a089
Matthew Wacha <matthewacha@gmail.com>
2019-03-22 09:58:46 +0300

    Revert "Setup heroku deployment"

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,13 @@ import ConnectedDash from '../views/Dashboard';
 import SiteLayout from '../hoc/Layout';
 
 
-
+// <Route
+// exact
+// path="/"
+// component={ConnectedAuth}
+// />
 const Routes = () => (
   <Switch>
-    <Route
-    exact
-    path="/"
-    component={ConnectedAuth}
-    />
     <Route>
       <SiteLayout>
         <Switch>
