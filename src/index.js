24d90949967ab3fba549c01a60e11839fcde196c
Matthew Wacha <matthewacha@gmail.com>
2019-03-22 09:58:46 +0300

    Revert "Setup heroku deployment"

--- src/index.js
+++ src/index.js
@@ -4,11 +4,5 @@ import './index.scss';
 import App from './views/App';
 import registerServiceWorker from './registerServiceWorker';
 
-
-const element = document.createElement('meta')
-element.name = "google-signin-client_id"
-element.content = process.env.REACT_APP_GOOGLE_CONTENT
-document.head.appendChild(element)
-
 render(<App />, document.getElementById('root'));
 registerServiceWorker();
